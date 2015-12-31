f2d8d5f3d76b44355d3466d11daa9b2278a1bd72
Suyeol Jeon <devxoul@gmail.com>
2016-01-01 02:03:52 +0900

    Xcode 프로젝트 시작

---
+++ Meal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meal
+//
+//  Created by 전수열 on 1/1/16.
+//  Copyright © 2016 Suyeol Jeon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
