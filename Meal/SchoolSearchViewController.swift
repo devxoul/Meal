9dbfc11853b9f68f0b7604f54e1c9d6989d787b0
Suyeol Jeon <devxoul@gmail.com>
2016-01-01 23:25:20 +0900

    SchoolSearchViewController 추가

---
+++ Meal/SchoolSearchViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SchoolSearchViewController.swift
+//  Meal
+//
+//  Created by 전수열 on 1/1/16.
+//  Copyright © 2016 Suyeol Jeon. All rights reserved.
+//
+
+import UIKit
+
+class SchoolSearchViewController: UIViewController {
+
+}
