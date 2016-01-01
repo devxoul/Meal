67af2dbafebd84828982fd7c42c197af13c04e55
Suyeol Jeon <devxoul@gmail.com>
2016-01-01 17:58:14 +0900

    UIWindow를 AppDelegate에서 생성

--- Meal/AppDelegate.swift
+++ Meal/AppDelegate.swift
@@ -15,7 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        self.window?.makeKeyAndVisible()
+        self.window?.rootViewController = ViewController()
         return true
     }
 
