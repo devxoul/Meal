491e4215f8c43816392a09d8ea09a8e335b707f1
Suyeol Jeon <devxoul@gmail.com>
2016-01-01 23:23:10 +0900

    MealListViewController를 네비게이션 컨트롤러로 감싸서 이용

--- Meal/AppDelegate.swift
+++ Meal/AppDelegate.swift
@@ -17,7 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
         self.window?.makeKeyAndVisible()
-        self.window?.rootViewController = MealListViewController()
+
+        let mealListViewController = MealListViewController()
+        let navigationController = UINavigationController(rootViewController: mealListViewController)
+        self.window?.rootViewController = navigationController
+
         return true
     }
 
