c64938bfb6ab65ead6daef27372ad5bdc72c7ff1
Suyeol Jeon <devxoul@gmail.com>
2016-01-01 18:55:22 +0900

    MealCell 추가

---
+++ Meal/MealCell.swift
@@ -0,0 +1,21 @@
+//
+//  MealCell.swift
+//  Meal
+//
+//  Created by 전수열 on 1/1/16.
+//  Copyright © 2016 Suyeol Jeon. All rights reserved.
+//
+
+import UIKit
+
+class MealCell: UITableViewCell {
+
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
