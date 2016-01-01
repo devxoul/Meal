6d0f1f61466e29fe5e6f0dbe7a8569ed7f94d2c5
Suyeol Jeon <devxoul@gmail.com>
2016-01-01 23:27:22 +0900

    학교 선택 화면에 제목과 배경색 추가

--- Meal/SchoolSearchViewController.swift
+++ Meal/SchoolSearchViewController.swift
@@ -10,4 +10,18 @@ import UIKit
 
 class SchoolSearchViewController: UIViewController {
 
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nil, bundle: nil)
+        self.title = "학교 선택"
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .whiteColor()
+    }
+
 }
