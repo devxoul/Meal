a0dedfa61eacd330f828db379714b08a6b8080f9
Suyeol Jeon <devxoul@gmail.com>
2016-01-01 18:58:23 +0900

    MealCell에 titleLabel과 contentLabel 추가

--- Meal/MealCell.swift
+++ Meal/MealCell.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class MealCell: UITableViewCell {
 
+    let titleLabel = UILabel()
+    let contentLabel = UILabel()
+
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         self.selectionStyle = .None
+
+        self.titleLabel.font = UIFont.boldSystemFontOfSize(15)
+        self.contentLabel.font = UIFont.systemFontOfSize(15)
+        self.contentLabel.numberOfLines = 0
+
+        self.contentView.addSubview(self.titleLabel)
+        self.contentView.addSubview(self.contentLabel)
     }
 
     required init?(coder aDecoder: NSCoder) {
