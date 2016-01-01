37ff8905d5a3a7c394a1ebdb91d36009d529cee4
Suyeol Jeon <devxoul@gmail.com>
2016-01-01 23:31:46 +0900

    School 모델 추가

---
+++ Meal/School.swift
@@ -0,0 +1,14 @@
+//
+//  School.swift
+//  Meal
+//
+//  Created by 전수열 on 1/1/16.
+//  Copyright © 2016 Suyeol Jeon. All rights reserved.
+//
+
+struct School {
+    var code: String
+    var type: String
+    var name: String
+    var address: String
+}
