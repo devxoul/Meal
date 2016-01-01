459ca3039000dcdc7657ec7ff2768e20e866b262
Suyeol Jeon <devxoul@gmail.com>
2016-01-01 23:45:09 +0900

    SchoolCell 추가

---
+++ Meal/SchoolCell.swift
@@ -0,0 +1,21 @@
+//
+//  SchoolCell.swift
+//  Meal
+//
+//  Created by 전수열 on 1/1/16.
+//  Copyright © 2016 Suyeol Jeon. All rights reserved.
+//
+
+import UIKit
+
+class SchoolCell: UITableViewCell {
+
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: .Subtitle, reuseIdentifier: reuseIdentifier)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
