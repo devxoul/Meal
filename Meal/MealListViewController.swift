f141e04c93afc2ee026eeca27df8a243516c3d3c
Suyeol Jeon <devxoul@gmail.com>
2016-01-01 23:23:18 +0900

    MealListViewController에 제목 추가

--- Meal/MealListViewController.swift
+++ Meal/MealListViewController.swift
@@ -14,6 +14,15 @@ class MealListViewController: UIViewController {
     let tableView = UITableView()
     var meals = [Meal]()
 
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nil, bundle: nil)
+        self.title = "선린인터넷고등학교"
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
