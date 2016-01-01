542299fe5ad687890798462c6e14acddac51685f
Suyeol Jeon <devxoul@gmail.com>
2016-01-01 18:16:36 +0900

    Meal 모델 추가

---
+++ Meal/Meal.swift
@@ -0,0 +1,13 @@
+//
+//  Meal.swift
+//  Meal
+//
+//  Created by 전수열 on 1/1/16.
+//  Copyright © 2016 Suyeol Jeon. All rights reserved.
+//
+
+struct Meal {
+    var date: String
+    var lunch: [String]
+    var dinner: [String]
+}
